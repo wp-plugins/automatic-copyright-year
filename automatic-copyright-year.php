c48b3187c3d5d60eaf64769820ffb34772395ae6
stubgo <stubgo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-21 01:45:32 +0000

    Add missing links
    git-svn-id: https://plugins.svn.wordpress.org/automatic-copyright-year/trunk@1269963 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- automatic-copyright-year.php
+++ automatic-copyright-year.php
@@ -29,4 +29,18 @@ function wpsos_include_scripts(){
 	wp_enqueue_script( 'wpsos-copyright', plugin_dir_url( __FILE__ ) . 'script.js', array('jquery'), '1.0', true);
 }
 add_action( 'wp_enqueue_scripts', 'wpsos_include_scripts' );
+
+/**
+ * Add links to WPSOS
+ */
+function wpsos_acy_set_plugin_meta( $links, $file ) {
+
+	if ( strpos( $file, 'automatic-copyright-year.php' ) !== false ) {
+
+		$links = array_merge( $links, array( '<a href="http://www.wpsos.io/wordpress-plugin-automatic-copyright-year/">' . __( 'Plugin details', 'simple-embed-code' ) . '</a>' ) );
+		$links = array_merge( $links, array( '<a href="http://www.wpsos.io/">' . __( 'WPSOS - WordPress Security, Optimization & Speed', 'simple-embed-code' ) . '</a>' ) );
+	}
+	return $links;
+}
+add_filter( 'plugin_row_meta', 'wpsos_acy_set_plugin_meta', 10, 2 );
 ?>
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: citywanderer, stubgo
 Donate link: http://www.wpsos.io/wordpress-plugin-automatic-copyright-year/
 Tags: copyright,shortcode,copyright year,footer copyright,widget
 Requires at least: 3.0.1
-Tested up to: 4.2.2
+Tested up to: 4.3
 Stable tag: 1.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
